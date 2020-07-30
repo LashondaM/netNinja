99eacd6116f385c041f09cef4cff38de94edac00
Lashonda Miller <lashondamiller007@gmail.com>
2020-07-29 22:46:38 -0400

    happy bruh

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import Ninjas from './ninjas';
+import AddNinja from './AddNinja';
 // './ninjas' is the file name for ninjas.js when we import that file to this current file
 
 class App extends Component {
+  state = {
+    ninjas : [
+      {name: 'pills', age: 19, afro: 'yellow', id: 1},
+      {name: 'and', age: 39, afro: 'pink', id: 2},
+      {name: 'potions', age: 48, afro: 'blue', id: 3}
+    ]
+  }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas: ninjas
+    })
+  }
   render() {
   return (
     <div className="App">
       <h1>Clowns</h1>
       <p>Welcome losers</p>
-      <Ninjas name="lashy" age="90" afro="dinosaur"/> <br />
-      <Ninjas name="yogi" age="55" afro="rainbow"/>
+      <Ninjas ninjas={this.state.ninjas} />
+      <AddNinja addNinja={this.addNinja} />
     </div>
    );
   }
--- my-app/src/ninjas.js
+++ my-app/src/ninjas.js
@@ -1,18 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component{
-    render(){
+const Ninjas = ({ninjas}) => {
         // console.log(this.props);
-        const {name, age, afro} = this.props;
+        // const ninjaList = ninjas.map(ninja => {
+        //     if (ninja.age > 20){
+        //     return (
+        //     <div className="ninja" key={ninja.id}>
+        //         <div>Name: { ninja.name }</div>
+        //         <div>Age: { ninja.age }</div>
+        //         <div>Afro: { ninja.afro }</div>
+        //     </div>
+        //     )
+        //     } else{
+        //         return null
+        //     }
+        // })
+        
         return(
-            <div className="ninja">
-                <div>Name: { name }</div>
-                <div>Age: { age }</div>
-                <div>Afro: { afro }</div>
-            </div>
+           <div className="ninja-list">
+               { 
+               ninjas.map(ninja => {
+            return ninja.age > 20 ? (
+                <div className="ninja" key={ninja.id}>
+                     <div>Name: { ninja.name }</div>
+                     <div>Age: { ninja.age }</div>
+                     <div>Afro: { ninja.afro }</div>
+                </div>
+            ) : null;
+        })
+      }
+           </div>
         )
     }
-}
 
 export default Ninjas
 //NINJAS IS THE NAME OF THE CLASS THAT WE EXPORT, ALWAYS USE THE NAME OF THE CLASS WHEN EXPORTING WITH REACT TILL FURTHER NOTICE
+// videos 14, 15, 18 of netNinja React and Redux,  watch it
