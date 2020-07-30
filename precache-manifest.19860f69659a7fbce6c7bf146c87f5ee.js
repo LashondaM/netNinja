5f45a991bc8b9954c4e19b11f9544584e8b9592b
Lashonda Miller <lashondamiller007@gmail.com>
2020-07-30 10:29:51 -0400

    Updates

---
+++ precache-manifest.19860f69659a7fbce6c7bf146c87f5ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200edb9ded279500784f8240063f0bf2",
+    "url": "/netNinja/index.html"
+  },
+  {
+    "revision": "36a7dcee375ca7d8cbf0",
+    "url": "/netNinja/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "82cfb8f7ca178e59503e",
+    "url": "/netNinja/static/js/2.8e080fb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/netNinja/static/js/2.8e080fb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a7dcee375ca7d8cbf0",
+    "url": "/netNinja/static/js/main.3ae03fd0.chunk.js"
+  },
+  {
+    "revision": "8ad5fbf4248fb211f879",
+    "url": "/netNinja/static/js/runtime-main.6768e0a0.js"
+  }
+]);
